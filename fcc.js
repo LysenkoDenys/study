3a59e3d196aba97738668b9b0de61a2543f2d2f0
Lysenko <lysenkoden@gmail.com>
2023-05-31 21:27:57 +0200

    Cash Register-2023-05-31

--- fcc.js
+++ fcc.js
@@ -4602,23 +4602,393 @@
 // palindrome("five|_/|four"); //should return false.
 
 // ========================================================================================
-// Roman Numeral Converter
-// Convert the given number into a roman numeral.
-// Roman numerals	Arabic numerals
-// M	1000
-// CM	900
-// D	500
-// CD	400
-// C	100
-// XC	90
-// L	50
-// XL	40
-// X	10
-// IX	9
-// V	5
-// IV	4
-// I	1
-// suggesed variant================================
-// suggesed variant================================
+// // Roman Numeral Converter
+// // Convert the given number into a roman numeral.
+// // Roman numerals	Arabic numerals
+// // M	1000
+// // CM	900
+// // D	500
+// // CD	400
+// // C	100
+// // XC	90
+// // L	50
+// // XL	40
+// // X	10
+// // IX	9
+// // V	5
+// // IV	4
+// // I	1
+
+// function convertToRoman(num) {
+//   const array = [
+//     ["M", 1000],
+//     ["CM", 900],
+//     ["D", 500],
+//     ["CD", 400],
+//     ["C", 100],
+//     ["XC", 90],
+//     ["L", 50],
+//     ["XL", 40],
+//     ["X", 10],
+//     ["IX", 9],
+//     ["V", 5],
+//     ["IV", 4],
+//     ["I", 1],
+//   ];
+//   const arrRoman = [];
+//   const arrRoman2 = [];
+//   let num2 = num;
+//   let result = "";
+
+//   while (num2 >= 0) {
+//     for (let index = 0; index < array.length; index++) {
+//       const numArab = array[index][1]; // 1000
+//       const numRoman = array[index][0]; // M
+//       const temp = num2 - numArab;
+
+//       if (temp >= 0) {
+//         arrRoman.push(numArab);
+//         arrRoman2.push(numRoman);
+//       }
+//     }
+//     num2 = num2 - arrRoman[0];
+//     arrRoman2[0] !== undefined ? (result = result + arrRoman2[0]) : "";
+
+//     // reset temporary array
+//     arrRoman.length = 0;
+//     arrRoman2.length = 0;
+//   }
+//   console.log(result); //XXXVI
+//   return result;
+// }
+
+// // suggesed variant================================
+// function convertToRoman(num) {
+//   var ref = [
+//     ["M", 1000],
+//     ["CM", 900],
+//     ["D", 500],
+//     ["CD", 400],
+//     ["C", 100],
+//     ["XC", 90],
+//     ["L", 50],
+//     ["XL", 40],
+//     ["X", 10],
+//     ["IX", 9],
+//     ["V", 5],
+//     ["IV", 4],
+//     ["I", 1],
+//   ];
+//   var res = [];
+//   ref.forEach(function (p) {
+//     while (num >= p[1]) {
+//       res.push(p[0]);
+//       num -= p[1];
+//     }
+//   });
+//   return res.join("");
+// }
+// // suggesed variant================================
+// convertToRoman(36); //XXXVI
+// convertToRoman(2); //should return the string II.
+// convertToRoman(3); //should return the string III.
+// convertToRoman(4); //should return the string IV.
+// convertToRoman(5); //should return the string V.
+// convertToRoman(9); //should return the string IX.
+// convertToRoman(12); //should return the string XII.
+// convertToRoman(16); //should return the string XVI.
+// convertToRoman(29); //should return the string XXIX.
+// convertToRoman(44); //should return the string XLIV.
+// convertToRoman(45); //should return the string XLV.
+// convertToRoman(68); //should return the string LXVIII
+// convertToRoman(83); //should return the string LXXXIII
+// convertToRoman(97); //should return the string XCVII
+// convertToRoman(99); //should return the string XCIX
+// convertToRoman(400); //should return the string CD
+// convertToRoman(500); //should return the string D
+// convertToRoman(501); //should return the string DI
+// convertToRoman(649); //should return the string DCXLIX
+// convertToRoman(798); //should return the string DCCXCVIII
+// convertToRoman(891); //should return the string DCCCXCI
+// convertToRoman(1000); //should return the string M
+// convertToRoman(1004); //should return the string MIV
+// convertToRoman(1006); //should return the string MVI
+// convertToRoman(1023); //should return the string MXXIII
+// convertToRoman(2014); //should return the string MMXIV
+// convertToRoman(3999); //should return the string MMMCMXCIX
 
 // ========================================================================================
+// // Caesars Cipher
+// // One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift cipher.
+// // In a shift cipher the meanings of the letters are shifted by some set amount.
+// // A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places.
+// // Thus A ↔ N, B ↔ O and so on.
+// // Write a function which takes a ROT13 encoded string as input and returns a decoded string.
+// // All letters will be uppercase.
+// // Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
+
+// // !PLAN:
+// // 1. create array of ABC;
+// // 2. convert string to array;;
+// // 3. create a vatiable to shift;
+// // 4. map the array to vatiable;
+// // 5. convert the array to a string;
+
+// function rot13(str) {
+//   const arrABC = [
+//     "A",
+//     "B",
+//     "C",
+//     "D",
+//     "E",
+//     "F",
+//     "G",
+//     "H",
+//     "I",
+//     "J",
+//     "K",
+//     "L",
+//     "M",
+//     "N",
+//     "O",
+//     "P",
+//     "Q",
+//     "R",
+//     "S",
+//     "T",
+//     "U",
+//     "V",
+//     "W",
+//     "X",
+//     "Y",
+//     "Z",
+//   ];
+//   const arrStr = str.toUpperCase().split("");
+//   const keyNumber = 13;
+
+//   const arrCipher = arrStr.map(function (element) {
+//     if (arrABC.indexOf(element) >= 0) {
+//       if (arrABC.indexOf(element) - keyNumber < 0) {
+//         return arrABC[arrABC.indexOf(element) - keyNumber + arrABC.length];
+//       } else {
+//         return arrABC[arrABC.indexOf(element) - keyNumber];
+//       }
+//     } else {
+//       return element;
+//     }
+//   });
+//   console.log(arrCipher.join("")); //
+//   return arrCipher.join("");
+// }
+
+// // suggesed variant================================
+// // Using modulus operator; makes the sentence uppercase;
+// function rot13(str) {
+//   str = str.toUpperCase();
+//   return str.replace(/[A-Z]/g, rot13);
+
+//   function rot13(correspondance) {
+//     const charCode = correspondance.charCodeAt();
+//     //A = 65, Z = 90
+//     return String.fromCharCode(
+//       charCode + 13 <= 90 ? charCode + 13 : ((charCode + 13) % 90) + 64
+//     );
+//   }
+// }
+// // suggesed variant================================
+// rot13("SERR PBQR PNZC"); //should decode to the string FREE CODE CAMP
+// rot13("SERR CVMMN!"); //should decode to the string FREE PIZZA!
+// rot13("SERR YBIR?"); //should decode to the string FREE LOVE?
+// rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT."); //should decode to the string THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
+
+// ========================================================================================
+// // Telephone Number Validator
+// // Return true if the passed string looks like a valid US phone number.
+// // The user may fill out the form field any way they choose as long as it has the format of a valid US number.
+// // The following are examples of valid formats for US numbers (refer to the tests below for other variants):
+// // 555-555-5555
+// // (555)555-5555
+// // (555) 555-5555
+// // 555 555 5555
+// // 5555555555
+// // 1 555 555 5555
+// // For this challenge you will be presented with a string such as 800-692-7753 or 8oo-six427676;laskdjf.
+// // Your job is to validate or reject the US phone number based on any combination of the formats provided above.
+// // The area code is required.
+// // If the country code is provided, you must confirm that the country code is 1.
+// // Return true if the string is a valid US phone number; otherwise return false.
+
+// function telephoneCheck(str) {
+//   const regExp =
+//     /^[^15(]|^1\d|^\([^15]|^\d{2}\s|^[^(]?\d{3}\)|[a-zA-Z?]|[^(]\d{3}\)|\(\d{3}[^)]|\)$/g;
+//   if (regExp.test(str) || str.length < 10) {
+//     console.log(false); //
+//     return false;
+//   }
+//   console.log(true); //
+//   return true;
+// }
+// // suggesed variant================================
+// function telephoneCheck(str) {
+//   return /^(1\s|1)?(\(\d{3}\)|\d{3})(-|\s)?\d{3}(-|\s)?\d{4}$/.test(str);
+// }
+// // suggesed variant================================
+// var re =
+//   /^([+]?1[\s]?)?((?:[(](?:[2-9]1[02-9]|[2-9][02-8][0-9])[)][\s]?)|(?:(?:[2-9]1[02-9]|[2-9][02-8][0-9])[\s.-]?)){1}([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2}[\s.-]?){1}([0-9]{4}){1}$/;
+
+// function telephoneCheck(str) {
+//   return re.test(str);
+// }
+// // suggesed variant================================
+
+// telephoneCheck("555-555-5555"); //should return a boolean.
+// telephoneCheck("1 555-555-5555"); //should return true.
+// telephoneCheck("1 (555) 555-5555"); //should return true.
+// telephoneCheck("5555555555"); //should return true.
+// telephoneCheck("555-555-5555"); //should return true.
+// telephoneCheck("(555)555-5555"); //should return true.
+// telephoneCheck("1(555)555-5555"); //should return true.
+// telephoneCheck("555-5555"); //should return false.
+// telephoneCheck("5555555"); //should return false.
+// telephoneCheck("1 555)555-5555"); //should return false.
+// telephoneCheck("1 555 555 5555"); //should return true.
+// telephoneCheck("1 456 789 4444"); //should return true.
+// telephoneCheck("123**&!!asdf#"); //should return false.
+// telephoneCheck("55555555"); //should return false.
+// telephoneCheck("(6054756961)"); //should return false.
+// telephoneCheck("2 (757) 622-7382"); //should return false.
+// telephoneCheck("0 (757) 622-7382"); //should return false.
+// telephoneCheck("-1 (757) 622-7382"); //should return false.
+// telephoneCheck("2 757 622-7382"); //should return false.
+// telephoneCheck("10 (757) 622-7382"); //should return false.
+// telephoneCheck("27576227382"); //should return false.
+// telephoneCheck("(275)76227382"); //should return false.
+// telephoneCheck("2(757)6227382"); //should return false.
+// telephoneCheck("2(757)622-7382"); //should return false.
+// telephoneCheck("555)-555-5555"); //should return false.
+// telephoneCheck("(555-555-5555"); //should return false.
+// telephoneCheck("(555)5(55?)-5555"); //should return false.
+// telephoneCheck("55 55-55-555-5"); //should return false.
+// telephoneCheck("11 555-555-5555"); //should return false.
+
+// ========================================================================================
+// Cash Register
+// Design a cash register drawer function checkCashRegister() that accepts purchase price as the first argument (price),
+// payment as the second argument (cash), and cash-in-drawer (cid) as the third argument.
+// cid is a 2D array listing available currency.
+// The checkCashRegister() function should always return an object with a status key and a change key.
+// Return {status: "INSUFFICIENT_FUNDS", change: []} if cash-in-drawer is less than the change due, or if you cannot return the exact change.
+// Return {status: "CLOSED", change: [...]} with cash-in-drawer as the value for the key change if it is equal to the change due.
+// Otherwise, return {status: "OPEN", change: [...]}, with the change due in coins and bills, sorted in highest to lowest order, as the value of the change key.
+
+// Currency Unit	Amount
+// Penny	$0.01 (PENNY)
+// Nickel	$0.05 (NICKEL)
+// Dime	$0.1 (DIME)
+// Quarter	$0.25 (QUARTER)
+// Dollar	$1 (ONE)
+// Five Dollars	$5 (FIVE)
+// Ten Dollars	$10 (TEN)
+// Twenty Dollars	$20 (TWENTY)
+// One-hundred Dollars	$100 (ONE HUNDRED)
+// See below for an example of a cash-in-drawer array:
+// [
+//   ["PENNY", 1.01],
+//   ["NICKEL", 2.05],
+//   ["DIME", 3.1],
+//   ["QUARTER", 4.25],
+//   ["ONE", 90],
+//   ["FIVE", 55],
+//   ["TEN", 20],
+//   ["TWENTY", 60],
+//   ["ONE HUNDRED", 100],
+// ];
+
+// !PLAN:
+// 1. create array of cid;
+// 2. check if cash>price and determine the currency in cid;
+// 3. check if cash>price;
+// 4. check if cash-price >= the currency incan cid and have we the same currency to payback;
+// 5. check if  we have some money in the cid;
+// 6. return object with { status: "", change: "" } with change sorted by value amount;
+
+function checkCashRegister(price, cash, cid) {
+  let change = { status: "", change: "" };
+  console.log(price); //
+  console.log(cash); //
+  console.log(cid); //
+  if (price > cash) {
+    return console.log("there are no money for purchace"); //
+  } else {
+    const payback = cash - price;
+    const cashInCid =
+      Math.round(
+        cid.reduce((previous, current) => {
+          return previous + current[1];
+        }, 0) * 100
+      ) / 100;
+    console.log(payback); //
+    console.log(cashInCid); //
+    if (cashInCid > payback && payback) {
+      change.status = "OPEN";
+    }
+  }
+  console.log(change); //
+  return change;
+}
+
+checkCashRegister(19.5, 20, [
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100],
+]); //should return {status: "OPEN", change: [["QUARTER", 0.5]]}.
+// checkCashRegister(3.26, 100, [
+//   ["PENNY", 1.01],
+//   ["NICKEL", 2.05],
+//   ["DIME", 3.1],
+//   ["QUARTER", 4.25],
+//   ["ONE", 90],
+//   ["FIVE", 55],
+//   ["TEN", 20],
+//   ["TWENTY", 60],
+//   ["ONE HUNDRED", 100],
+// ]); //should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
+// checkCashRegister(19.5, 20, [
+//   ["PENNY", 0.01],
+//   ["NICKEL", 0],
+//   ["DIME", 0],
+//   ["QUARTER", 0],
+//   ["ONE", 0],
+//   ["FIVE", 0],
+//   ["TEN", 0],
+//   ["TWENTY", 0],
+//   ["ONE HUNDRED", 0],
+// ]); //should return {status: "INSUFFICIENT_FUNDS", change: []}.
+// checkCashRegister(19.5, 20, [
+//   ["PENNY", 0.01],
+//   ["NICKEL", 0],
+//   ["DIME", 0],
+//   ["QUARTER", 0],
+//   ["ONE", 1],
+//   ["FIVE", 0],
+//   ["TEN", 0],
+//   ["TWENTY", 0],
+//   ["ONE HUNDRED", 0],
+// ]); //should return {status: "INSUFFICIENT_FUNDS", change: []}.
+// checkCashRegister(19.5, 20, [
+//   ["PENNY", 0.5],
+//   ["NICKEL", 0],
+//   ["DIME", 0],
+//   ["QUARTER", 0],
+//   ["ONE", 0],
+//   ["FIVE", 0],
+//   ["TEN", 0],
+//   ["TWENTY", 0],
+//   ["ONE HUNDRED", 0],
+// ]); //should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}.
