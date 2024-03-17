7adb0de2f51de50c3ae15f63c00074443dd51167
Lysenko <lysenkoden@gmail.com>
2024-03-17 09:39:32 +0100

    Follow that Spy

--- codewars.js
+++ codewars.js
@@ -2724,6 +2724,79 @@ console.log(nico('ba', '1234567890')); //"2143658709"
 console.log(nico('p2xl8raoie', '44r55k6w9mb')); //"456m95w4kr       b  "
 // ===============================================================================================================================================
 
+// 6 kyu
+// Follow that Spy
+// We are diligently pursuing our elusive operative, Matthew Knight, who also goes by the alias Roy Miller.
+// He employs a nomadic lifestyle to evade detection, constantly moving from one location to another, with each of his journeys following a perplexing and non-standard sequence of itineraries.
+// Our mission is to decipher the routes he will undertake during each of his voyages.
+
+// Task
+// You've been provided with an array of itinerary routes, decipher the precise destinations he will visit in the correct sequence according to his meticulously planned itineraries.
+
+// Example
+// Based on the provided routes:
+
+// [ [USA, BRA], [JPN, PHL], [BRA, UAE], [UAE, JPN] ]
+// The correct sequence of destinations is:
+
+// "USA, BRA, UAE, JPN, PHL"
+// Note:
+
+// You can safely assume that there will be no duplicate locations with distinct routes.
+// All routes provided will have non-empty itineraries.
+// There will always be at least one (1) route connecting one waypoint to another.
+
+// we need to find initial point
+
+function findRoutes(routes) {
+  let initialPoint = [...routes];
+
+  for (let i = 0; i < routes.length; i++) {
+    for (let j = 0; j < routes.length; j++) {
+      if (routes[i][0] === routes[j][1]) {
+        initialPoint = initialPoint.filter((item) => item !== routes[i]);
+      }
+    }
+  }
+
+  const arrTofill = [...initialPoint];
+
+  for (let i = 0; i < routes.length - 1; i++) {
+    if (routes.find((el) => el[0] === arrTofill[arrTofill.length - 1][1])) {
+      arrTofill.push(
+        routes.find((el) => el[0] === arrTofill[arrTofill.length - 1][1])
+      );
+    }
+  }
+
+  const result = [].concat(...arrTofill);
+  //recursion should be used because `[].concat(...arrTofill)` - just for 1 level fletting:
+  return [...new Set(result)].join(', ');
+}
+
+//best practice:
+
+console.log(
+  findRoutes([
+    ['MNL', 'TAG'],
+    ['CEB', 'TAC'],
+    ['TAG', 'CEB'],
+    ['TAC', 'BOR'],
+  ])
+); // "MNL, TAG, CEB, TAC, BOR"
+console.log(
+  findRoutes([
+    ['Chicago', 'Winnipeg'],
+    ['Halifax', 'Montreal'],
+    ['Montreal', 'Toronto'],
+    ['Toronto', 'Chicago'],
+    ['Winnipeg', 'Seattle'],
+  ])
+); // "Halifax, Montreal, Toronto, Chicago, Winnipeg, Seattle"
+// ===============================================================================================================================================
+//best practice:
+
+// ===============================================================================================================================================
 //best practice:
 
 // ===============================================================================================================================================
---
+++ other.js
@@ -0,0 +1,12 @@
+// https://www.youtube.com/watch?v=FVbr0CS4oR8&ab_channel=%D0%A1%D0%B5%D1%80%D0%B3%D1%96%D0%B9%D0%91%D0%B0%D0%B1%D1%96%D1%87%D1%82%D0%B0%D0%94%D0%B8%D0%B2%D0%BE%D0%B2%D0%B8%D0%B6%D0%BD%D0%B8%D0%B9%D1%81%D0%B2%D1%96%D1%82%D0%B2%D0%B5%D0%B1%D1%80%D0%BE%D0%B7%D1%80%D0%BE%D0%B1%D0%BA%D0%B8
+
+//write async function:
+// 1 get a list of items '{name: string, price: number}'
+// 2 group by value for 4 category:
+// 1-100, 101-200, 200-500, 500+
+// 3. sort it by name (the ignored)
+// 4 calculate average price by each category.
+
+async function fetchAndGroupItems(params) {
+  return;
+}
