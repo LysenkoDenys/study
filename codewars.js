57831c84837883e9817f9f9db11c3a25bf715ccf
Lysenko <lysenkoden@gmail.com>
2024-03-21 22:18:23 +0100

    Running out of space

--- codewars.js
+++ codewars.js
@@ -2794,8 +2794,28 @@ console.log(
   ])
 ); // "Halifax, Montreal, Toronto, Chicago, Winnipeg, Seattle"
 // ===============================================================================================================================================
-//best practice:
+// 7 kyu
+// Running out of space
 
+// Kevin is noticing his space run out! Write a function that removes the spaces from the values and returns an array showing the space decreasing.
+// For example, running this function on the array ['i', 'have','no','space'] would produce ['i','ihave','ihaveno','ihavenospace']
+
+function spacey(array) {
+  const arrNew = [array[0]];
+  for (let i = 1; i < array.length; i++) {
+    const element = arrNew[i - 1] + array[i];
+    arrNew.push(element);
+  }
+  return arrNew;
+}
+
+//best practice:
+function spacey(array) {
+  let string = '';
+  return array.map((e) => (string += e));
+}
+
+console.log(spacey(['kevin', 'has', 'no', 'space'])); //['kevin','kevinhas','kevinhasno','kevinhasnospace']
 // ===============================================================================================================================================
 //best practice:
 
